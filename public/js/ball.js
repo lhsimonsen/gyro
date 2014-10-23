14c48085333a877cb19f39cabb45872f41db6e31
Lars Haakon Simonsen <lh.simonsen@gmail.com>
2014-10-23 22:52:06 +0200

    added friction and boundaries

--- public/js/ball.js
+++ public/js/ball.js
@@ -1,15 +1,48 @@
 var Ball = Ball || (function() {
 	'use strict'
 
-	function Ball(x ,y) {
+	function Ball(x ,y, radius) {
 		this.x = x;
 		this.y = y;
+		this.radius = radius;
+		this.velocity = {x: 0, y: 0}
+		this.friction = 0.98;
 	}
 
-	Ball.prototype.update = function(speed) {
-		this.speed = speed;
-		this.x = this.x + (this.speed.xv);
-		this.y = this.y + (this.speed.yv);
+	Ball.prototype.update = function(axis) {
+		this.axis = axis;
+
+		if(this.velocity.y > this.axis.y)
+			this.velocity.y--;
+		if(this.velocity.y < this.axis.y)
+			this.velocity.y++;
+		if(this.velocity.x > this.axis.x)
+			this.velocity.x--;
+		if(this.velocity.x < this.axis.x)
+			this.velocity.x++;
+
+		this.velocity.y *= this.friction;
+		this.y += this.velocity.y;
+		this.velocity.x *= this.friction;
+		this.x += this.velocity.x;
+
+		if(this.x > (Gyro.canvas.width - 50))
+			this.x = Gyro.canvas.width - 50;
+		else if(this.x <= 50)
+			this.x = 50;
+
+		if(this.y > (Gyro.canvas.height - 50))
+			this.y = Gyro.canvas.height - 50;
+		else if(this.y <= 50)
+			this.y = 50;
+	};
+
+	Ball.prototype.draw = function() {
+		Gyro.context.beginPath();
+    	Gyro.context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+    	Gyro.context.closePath();
+    	Gyro.context.fillStyle = "#ff00ff";
+    	Gyro.context.fill();
 	}
 
 	return Ball;
--- public/js/stage.js
+++ public/js/stage.js
@@ -4,7 +4,7 @@ var Stage = Stage || (function() {
   function Stage() {
     document.body.appendChild(Gyro.canvas);
     this.setCanvasSize();
-    this.ball = new Ball(Gyro.canvas.width * 0.5, Gyro.canvas.height * 0.5);
+    this.ball = new Ball(Gyro.canvas.width * 0.5, Gyro.canvas.height * 0.5, 50);
     this.speed = {xv: 0, yv: 0};
   }
 
@@ -14,8 +14,8 @@ var Stage = Stage || (function() {
 
   Stage.prototype.update = function() {
     // document.getElementById('xaxis').innerHTML = Gyro.beta;
-    this.speed.xv = Gyro.gamma;
-    this.speed.yv = Gyro.beta;
+    this.speed.x = Gyro.gamma;
+    this.speed.y = Gyro.beta;
     this.ball.update(this.speed);
     this.render();
   };
@@ -24,8 +24,7 @@ var Stage = Stage || (function() {
     Gyro.context.clearRect(0, 0, this.width, this.height);
     Gyro.context.save();
 
-    Gyro.context.fillStyle = "#ff00ff";
-    Gyro.context.fillRect(this.ball.x, this.ball.y, 50, 50);
+    this.ball.draw();
 
     Gyro.context.translate(this.width * 0.5, this.height * 0.5);
     Gyro.context.restore();
